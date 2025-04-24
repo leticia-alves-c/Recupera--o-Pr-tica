aede5ce2effe97a04e45d90b02c0f20aea45ee11
LETÍCIA ALVES COELHO <leticia_a_coelho@estudante.sesisenai.org.br>
2025-04-24 09:04:33 -0300

    primeiro commit

---
+++ personagem.js
@@ -0,0 +1,43 @@
+
+class Personagem {
+    constructor(vida, força, mana) {
+        this.vida = vida; 
+        this.forca = força; 
+        this.mana = mana;
+    }
+
+    atacar() {
+        console.log(`O personagem atacou com força ${this.forca}`); 
+    }
+
+    usarMana(quantidade) {
+        if (this.mana >= quantidade) {
+            this.mana -= quantidade;
+            console.log(`Usou ${quantidade} de mana.`); 
+        } else {
+            console.log("Mana insuficiente.");
+        }
+    }
+
+    recuperarVida(quantidade) {
+        this.vida += quantidade; 
+        if (this.vida > 100) {
+            this.vida = 100; 
+        }
+        console.log(`Recuperou ${quantidade} de vida. Vida atual: ${this.vida}`);
+    }
+
+    apresentar() {
+        console.log(`Vida: ${this.vida}, Força: ${this.forca}, Mana: ${this.mana}`); 
+    }
+}
+
+
+const meuPersonagem = new Personagem(100, 30, 20); 
+console.log("Personagem criado:");
+meuPersonagem.apresentar(); 
+
+meuPersonagem.atacar(); 
+meuPersonagem.usarMana(20); 
+meuPersonagem.recuperarVida(30); 
+meuPersonagem.apresentar(); 
